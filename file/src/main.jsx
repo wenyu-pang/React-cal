34936f475eb92dae61cfe325ac5200b7c65a975d
wenyu-pang <pang599945622@gmail.com>
2025-04-28 17:26:30 -0400

    Add files via upload

---
+++ file/src/main.jsx
@@ -0,0 +1,11 @@
+
+import { createRoot } from 'react-dom/client'
+
+import App from './App.jsx'
+import DigitButton from './DigitButton.jsx'
+import OperationButton from './OperationButton.jsx'
+createRoot(document.getElementById('root')).render(
+
+    <App />
+
+)
